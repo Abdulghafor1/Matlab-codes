47e1db988a8e9a415a68de45ae07e6ac047dca01
Abdulghafor1 <142575431+Abdulghafor1@users.noreply.github.com>
2023-08-18 10:42:39 +0300

    Add files via upload

---
+++ matchmatical_series.m
@@ -0,0 +1,33 @@
+clc
+clear all
+% n=4;
+% for i=1:n
+%     x(i)=i^2/5;
+% end
+% y=sum(x);
+%%%%%%%%%
+%% even multiplication
+% n=5;
+% for i=1:n
+%     x(i)=2^(i-1);
+% end
+% y=sum(x)
+%% odd multiplication
+% n=5;
+% for i=1:n
+%     x(i)=2^(i)-1;
+% end
+% y=sum(x)
+%% odd numbers
+% n=5;
+% for i=1:n
+%     x(i)=2*(i)-1;
+% end
+% y=sum(x)
+%% even numbers 
+% n=5;
+% for i=1:n
+%     x(i)=2*(i);
+% end
+% y=sum(x)
+%%
